20780e48489f13be176880816ced7af37a1b5fd0
Azamatjon17 <153727489+Azamatjon17@users.noreply.github.com>
2024-06-08 15:10:55 +0500

    sd

--- lib/views/screens/clients_screen.dart
+++ lib/views/screens/clients_screen.dart
@@ -1,3 +1,4 @@
+import 'package:admin_panel/controllers/cilents_controller.dart';
 import 'package:flutter/material.dart';
 
 class ClientsScreen extends StatefulWidget {
@@ -8,14 +9,31 @@ class ClientsScreen extends StatefulWidget {
 }
 
 class _ClientsScreenState extends State<ClientsScreen> {
+  CilentsController cilentsController = CilentsController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-           title: Text(
+        title: const Text(
           "Clients",
         ),
         backgroundColor: Colors.blue.shade700,
+      ),
+      body: FutureBuilder(
+        future: cilentsController.cilentsHttpService.getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text("Malumot olishda xato  bor"),
+            );
+          }
+          if (!snapshot.hasData) {
+            return const Center(
+              child: Text("Malumot yo'q"),
+            );
+          }
+          return Center();
+        },
       ),
     );
   }
