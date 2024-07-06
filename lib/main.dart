243914daea8d7b922bbe68ab053fd16b50df9441
Bexruzbek-dev <behruz.toshpolatov14@gmail.com>
2024-07-06 13:23:48 +0500

    sdjfh

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main()async {
-   WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
